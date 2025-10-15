6a41b7dabf1a939222df0ea83eee0b3364baa2a7
Luis Miguel <luismikg@gmail.com>
2025-10-15 04:05:27 -0600

    :tada: Small news project

--- app/src/main/java/com/luis/smallnews/data/Constants.kt
+++ app/src/main/java/com/luis/smallnews/data/Constants.kt
@@ -3,6 +3,6 @@ package com.luis.smallnews.data
 object Constants {
 
     const val URL_BASE: String = "https://newsapi.org/"
-    const val API_KEY: String = "YOUR_APK"
+    const val API_KEY: String = "YOUR_API_KEY"
     const val TOP_HEADLINES_SERVICE = "v2/top-headlines"
 }
